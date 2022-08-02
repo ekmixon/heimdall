d3e69fc0b3520247d70498e85b1551c96e4cf0a3
Sourcery AI <>
2022-08-02 19:30:09 +0000

    'Refactored by Sourcery'

--- docs/oneTimeRun.py
+++ docs/oneTimeRun.py
@@ -18,13 +18,21 @@ with open(nameOfFile) as csvDataFile:
         hostNames.append(row[0])
         ipAddresses.append(row[1])
 
-if (scanType == "host name" or scanType =="hostname" or scanType =="Host name" or scanType =="Hostname"):
+if scanType in ["host name", "hostname", "Host name", "Hostname"]:
     for hostName in hostNames:    
-        inspecScan = "sudo inspec exec inspec-profile-disa_stig-el7/ --attrs=inspec-profile-disa_stig-el7/attributes.yml -t ssh://" + username + "@" + hostName + " --sudo --sudo-password=" + password + " --password=" + password + " --reporter cli json:" + currentPath + hostName + "-" + str(today) + ".json"
+        inspecScan = f"sudo inspec exec inspec-profile-disa_stig-el7/ --attrs=inspec-profile-disa_stig-el7/attributes.yml -t ssh://{username}@{hostName} --sudo --sudo-password={password} --password={password} --reporter cli json:{currentPath}{hostName}-{str(today)}.json"
+
         os.system(inspecScan)
-elif (scanType == "ip address" or scanType =="ipaddress" or scanType =="Ip Address" or scanType =="IP Address" or scanType =="IP address"):
+elif scanType in [
+    "ip address",
+    "ipaddress",
+    "Ip Address",
+    "IP Address",
+    "IP address",
+]:
     for ipAddress in ipAddresses:
-        inspecScan = "sudo inspec exec inspec-profile-disa_stig-el7/ --attrs=inspec-profile-disa_stig-el7/attributes.yml -t ssh://" + username + "@" + ipAddress + " --sudo --sudo-password=" + password + " --password=" + password + " --reporter cli json:" + currentPath + ipAddress + "-" + str(today) + ".json"
+        inspecScan = f"sudo inspec exec inspec-profile-disa_stig-el7/ --attrs=inspec-profile-disa_stig-el7/attributes.yml -t ssh://{username}@{ipAddress} --sudo --sudo-password={password} --password={password} --reporter cli json:{currentPath}{ipAddress}-{str(today)}.json"
+
         os.system(inspecScan)
 else:
     scanType = input("How would you like to run this scan, via host name or via ip address?")
